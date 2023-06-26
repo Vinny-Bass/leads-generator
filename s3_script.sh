affaa3dc7084ab2d736cf7570114b2b7f5b9e5ba
Vinicius Bass <raylee558@gmail.com>
2023-06-26 17:12:29 +0100

    add s3 location on env

--- s3_script.sh
+++ s3_script.sh
@@ -15,7 +15,7 @@ do
     num_rows=$(wc -l < "$file")
 
     if [ "$num_rows" -ge "$MAX_ROWS_PER_FILE" ]; then
-      aws s3 cp "$file" "s3://leads-892ec6b2-ea0e-4f8d-ad32-690bc209d3ac"
+      aws s3 cp "$file" "$S3_LOCATION"
       rm "$file"
     fi
   fi
